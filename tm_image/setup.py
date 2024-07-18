0e4f225bea8174b84e0316f237bf1de9aed95fdf
YH.Wang <yh.wang@tm-robot.com>
2024-07-18 14:26:24 +0800

    1.1.7 Modify all package.xml and setup.py to BSD-3-Clause license.

--- tm_image/setup.py
+++ tm_image/setup.py
@@ -17,7 +17,7 @@ setup(
     maintainer='leowu',
     maintainer_email='leo.wu@tm-robot.com',
     description='tm_image',
-    license='BSD',
+    license='BSD-3-Clause',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
